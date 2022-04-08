2047572eaf45cc6cb29b029ea90b145679c562c9
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-04-08 17:21:11 +0200

    Auction model created, added to Admin and 1 item created through admin

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,24 @@
+from unicodedata import category
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.forms import BooleanField
+from django.forms import DateTimeField
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+	title = models.CharField(max_length=50)
+	description = models.TextField(max_length=500)
+	startingbid = models.FloatField()
+	image = models.URLField()
+	category = models.CharField(max_length=15)
+	dateuploaded = models.DateTimeField()
+	isactive = models.BooleanField(default=True)
+
+
+
+# TO DO: 3 MODELS :
+# auction listing ALL NEEDED
+# bids
+# comments made on auction listings.
